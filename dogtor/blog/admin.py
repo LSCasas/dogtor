855a2efea76603fa814c4ff9af61c46cc4dcef56
Luis Casas <luisscasas45@gmail.com>
2025-01-19 13:09:40 -0600

    FEAT/ Add custom admin area and register Pet-related models

--- dogtor/blog/admin.py
+++ dogtor/blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+
+
 #Panel de administracion para la app de 'blog'
 class BlogAdminArea(admin.AdminSite):
     """Blog admin panel administracion"""
@@ -9,3 +13,11 @@ class BlogAdminArea(admin.AdminSite):
 
 #Instanciar nuestra clase
 blog_admin_site = BlogAdminArea(name="BlogAdmin")
+
+
+#registramos modelo 'Post' en nuestro admin area
+blog_admin_site.register(models.Post)
+
+
+#Registrarlo en el admin area general de admin
+admin.site.register(models.Post)
--- dogtor/vet/admin.py
+++ dogtor/vet/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+#Panel de administracion para la app de 'blog'
+
+class VetAdminArea(admin.AdminSite):
+    """Vet admin panel administracion"""
+    site_header = "Vet Site Admin Area"
+
+#Instanciar nuestra clase
+
+vet_admin_site = VetAdminArea(name="VetAdmin")
+
+
+#registramos modelo 'Post' en nuestro admin area
+vet_admin_site.register(models.PetOwner)
+vet_admin_site.register(models.Pet)
+vet_admin_site.register(models.PetDate)
+
+
+#Registrarlo en el admin area general de admin
+admin.site.register(models.PetOwner)
+admin.site.register(models.Pet)
+admin.site.register(models.PetDate)
